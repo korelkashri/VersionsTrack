19dad614abc89b3e175bc4d0d48f31f06d97887b
korelkashri <korelkashri@gmail.com>
2019-11-19 17:36:32 +0200

    Some progress with users login/register.

--- routers/api/users.js
+++ routers/api/users.js
@@ -3,7 +3,7 @@ let router = express.Router();
 const users_controller = require("../../controllers/users");
 con_validator = require('../../middlewares/validate_connection');
 
-router.get("/login", con_validator.require_logout, users_controller.login); // Validate login
+router.get("/login/:username", con_validator.require_logout, users_controller.login); // Validate login
 
 router.post("/register", con_validator.require_logout, users_controller.register); // Validate login
 
